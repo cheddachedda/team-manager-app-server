dd994806d971772b824595119264e6cea2b60773
Stephen <stephen.sant123@gmail.com>
2021-11-12 16:01:03 +1100

    Added three users to the seed date.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
-# User.destroy_all
-# User.create(email: => "kevin@email.com", name: => "Kevn");
+User.destroy_all
+u0 = User.create :email => "kevin@email.com", :name => "Kevn", :password => "chicken"
+u1 = User.create :email => "paul@email.com", :name => "Luap", :password => "chicken"
+u2 = User.create :email => "carl@email.com", :name => "Carl", :password => "chicken"
+puts "#{ User.count } users created."
