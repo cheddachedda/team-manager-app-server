347cbc465057b1a789cff203c6a1a797c1737920
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-12 17:14:13 +1100

    Log error messages when User#create fails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,17 @@ class UsersController < ApplicationController
 
 	def index
 		@users = User.all
-
 		render json: @users
 	end
 
 	def create
-
-	@user = User.create(user_params)
-
-	render json: @user, status: :created
+		@user = User.create(user_params)
+		if @user.save
+			render json: @user, status: :created
+		else
+			# Logs errors to the console if user was not created
+			render json: @user.errors.full_messages
+		end
 	end
 
   private
