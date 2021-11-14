841e0538f82c78a517ec97289b6f2541f9f75c90
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-14 12:57:13 +1100

    Add :password_confirmation after reading the full docs; mea culpa

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.destroy_all
-u0 = User.create :email => "kevin@email.com", :name => "Kevn", :password => "chicken", admin: false
-u1 = User.create :email => "paul@email.com", :name => "Luap", :password => "chicken", admin: false
-u2 = User.create :email => "carl@email.com", :name => "Carl", :password => "chicken", admin: false
-u3 = User.create :email => "team@email.com", :name => "Team", :password => "chicken", admin: true
+u0 = User.create :email => "kevin@email.com", :name => "Kevn", :password => "chicken", password_confirmation: "chicken", admin: false
+u1 = User.create :email => "paul@email.com", :name => "Luap", :password => "chicken", password_confirmation: "chicken", admin: false
+u2 = User.create :email => "carl@email.com", :name => "Carl", :password => "chicken", password_confirmation: "chicken", admin: false
+u3 = User.create :email => "team@email.com", :name => "Team", :password => "chicken", password_confirmation: "chicken", admin: true
 puts "#{ User.count } users created."
