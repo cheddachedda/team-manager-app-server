c01269ad94bef76c7946fa2ec47da629fa18bfb7
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-15 15:43:57 +1100

    Change associations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :email, :presence => true, :uniqueness => true
   has_secure_password
   validates :name, presence: true
-  has_one :team
-  belongs_to :game
+  belongs_to :team
+  has_and_belongs_to_many :game
 end
