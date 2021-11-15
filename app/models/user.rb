49be474790275460a51bb54e98ccf73e09cdf53f
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-16 10:42:37 +1100

    Allow User's team to be optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :email, :presence => true, :uniqueness => true
   has_secure_password
   validates :name, presence: true
-  belongs_to :team
-  has_and_belongs_to_many :game
+  belongs_to :team, optional: true
+  has_and_belongs_to_many :games
 end
