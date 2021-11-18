1da4237724f36890692cd2cf63fc02b03749e875
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-19 10:22:45 +1100

    Add ladder position

--- app/models/team.rb
+++ app/models/team.rb
@@ -51,6 +51,8 @@ class Team < ApplicationRecord
   end
 
   def league_position
+    teams = Team.where(division: 'mon_mixed').sort_by { |t| [t.win_percentage, t.points_percentage] }
+    teams.index(team + 1)
   end
 
 end
